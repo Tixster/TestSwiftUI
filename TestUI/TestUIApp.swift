da91c80a7639288c07bac6aa6c4350dfbbe69d27
Kirill Tila <gauter51@gmail.com>
2021-11-29 19:28:55 +0200

    Initial Commit

---
+++ TestUI/TestUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestUIApp.swift
+//  TestUI
+//
+//  Created by Кирилл Тила on 29.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
